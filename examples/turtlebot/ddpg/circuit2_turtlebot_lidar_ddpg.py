d7ea5fbfd6f60264b6a6fac26ee14c1d1be4c2da
Qirui Luo <qiruiluo2022@u.northwestern.edu>
2019-05-25 18:13:20 -0500

    DDPG test

--- examples/turtlebot/ddpg/circuit2_turtlebot_lidar_ddpg.py
+++ examples/turtlebot/ddpg/circuit2_turtlebot_lidar_ddpg.py
@@ -27,10 +27,11 @@ def clear_monitor_files(training_dir):
         os.unlink(file)
 
 EPISODES = 100000
-
+TEST = 10
 def main():
     #REMEMBER!: turtlebot_nn_setup.bash must be executed.
     env = gym.make('GazeboCircuit2TurtlebotLidarNn-v0')
+    agent = DDPG(env)
     outdir = '/tmp/gazebo_gym_experiments/'
     path = '/tmp/turtle_c2_dqn_ep'
     plotter = liveplot.LivePlot(outdir)
@@ -41,13 +42,36 @@ def main():
     resume_path = path + resume_epoch
     monitor_path = resume_path
 
-    clear_monitor_files(outdir)
-    copy_tree(monitor_path,outdir)
+    steps = 100000
 
-    agent = DDPG(env)
+    env._max_episode_steps = steps
+    # env = gym.wrappers.Monitor(env, outdir,force=not continue_execution, resume=continue_execution)
 
     for episode in xrange(EPISODES):
         observation = env.reset()
+        #print "episode:",episode
+        # Train
+        for step in xrange(env.spec.timestep_limit):
+            action = agent.noise_action(observation)
+            next_state,reward,done,_ = env.step(action)
+            agent.perceive(observation,action,reward,next_state,done)
+            state = next_state
+            if done:
+                break
+        # Testing:
+        if episode % 100 == 0 and episode > 100:
+            total_reward = 0
+            for i in xrange(TEST):
+                state = env.reset()
+                for j in xrange(env.spec.timestep_limit):
+                    #env.render()
+                    action = agent.action(state) # direct action for test
+                    state,reward,done,_ = env.step(action)
+                    total_reward += reward
+                    if done:
+                        break
+            ave_reward = total_reward/TEST
+            print ('episode: ',episode,'Evaluation Average Reward:',ave_reward)
 
     env.monitor.close()
 
