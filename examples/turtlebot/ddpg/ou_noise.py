5dd2ecae7261eaa984dc77bc98f6f287a4518e87
Qirui Luo <qiruiluo2022@u.northwestern.edu>
2019-06-04 08:12:14 -0500

    Update ou_noise.py

--- examples/turtlebot/ddpg/ou_noise.py
+++ examples/turtlebot/ddpg/ou_noise.py
@@ -10,7 +10,8 @@ import numpy.random as nr
 
 class OUNoise:
     """docstring for OUNoise"""
-    def __init__(self,action_dimension,mu=0, theta=0.015, sigma=0.02):
+    # def __init__(self,action_dimension,mu=0, theta=0.15, sigma=0.2):
+    def __init__(self,action_dimension,mu=0, theta=0, sigma=0):
         self.action_dimension = action_dimension
         self.mu = mu
         self.theta = theta
